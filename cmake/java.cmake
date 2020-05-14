2c2af31ee758695af68e7ff6ccae4cef41bf4e23
Mizux Seiha <mizux.dev@gmail.com>
2020-05-14 18:01:23 +0200

    cmake(java): Add Maven Package (#202)

--- cmake/java.cmake
+++ cmake/java.cmake
@@ -3,7 +3,7 @@ if(NOT BUILD_JAVA)
 endif()
 
 if(NOT TARGET ortools::ortools)
-  message(FATAL_ERROR "Java: missing ortools TARGET")
+  message(FATAL_ERROR "Java: missing ortools::ortools TARGET")
 endif()
 
 # Will need swig
@@ -48,12 +48,17 @@ foreach(PROTO_FILE ${proto_java_files})
 endforeach()
 add_custom_target(Java${PROJECT_NAME}_proto DEPENDS ${PROTO_JAVAS} ortools::ortools)
 
-# Setup Java
-find_package(Java 1.8 REQUIRED COMPONENTS Development)
+# Find Java
+find_package(Java 1.8 COMPONENTS Development REQUIRED)
 find_package(JNI REQUIRED)
 
 # Find maven
-find_program(MAVEN_EXECUTABLE mvn)
+# On windows mvn spawn a process while mvn.cmd is a blocking command
+if(UNIX)
+  find_program(MAVEN_EXECUTABLE mvn)
+else()
+  find_program(MAVEN_EXECUTABLE mvn.cmd)
+endif()
 if(NOT MAVEN_EXECUTABLE)
   message(FATAL_ERROR "Check for maven Program: not found")
 else()
@@ -77,48 +82,130 @@ set_target_properties(jniortools PROPERTIES
   POSITION_INDEPENDENT_CODE ON)
 # note: macOS is APPLE and also UNIX !
 if(APPLE)
-  set_target_properties(jniortools PROPERTIES INSTALL_RPATH "@loader_path")
+  set_target_properties(jniortools PROPERTIES
+    INSTALL_RPATH "@loader_path")
+  # Xcode fails to build if library doesn't contains at least one source file.
+  if(XCODE)
+    file(GENERATE
+      OUTPUT ${PROJECT_BINARY_DIR}/jniortools/version.cpp
+      CONTENT "namespace {char* version = \"${PROJECT_VERSION}\";}")
+    target_sources(jniortools PRIVATE ${PROJECT_BINARY_DIR}/jniortools/version.cpp)
+  endif()
 elseif(UNIX)
   set_target_properties(jniortools PROPERTIES INSTALL_RPATH "$ORIGIN")
 endif()
 
+# Needed by java/CMakeLists.txt
+set(JAVA_PACKAGE com.google.ortools)
+set(JAVA_PACKAGE_PATH src/main/java/com/google/ortools)
+set(JAVA_RESOURCES_PATH src/main/resources)
+if(APPLE)
+  set(NATIVE_IDENTIFIER darwin)
+elseif(UNIX)
+  set(NATIVE_IDENTIFIER linux-x86-64)
+elseif(WIN32)
+  set(NATIVE_IDENTIFIER win32-x86-64)
+else()
+  message(FATAL_ERROR "Unsupported system !")
+endif()
+set(JAVA_NATIVE_PROJECT ortools-${NATIVE_IDENTIFIER})
+set(JAVA_PROJECT ortools-java)
+
 # Swig wrap all libraries
-set(OR_TOOLS_JAVA com.google.ortools)
 foreach(SUBPROJECT IN ITEMS algorithms graph linear_solver constraint_solver sat util)
   add_subdirectory(ortools/${SUBPROJECT}/java)
-  target_link_libraries(jniortools PRIVATE java_${SUBPROJECT})
+  target_link_libraries(jniortools PRIVATE jni${SUBPROJECT})
 endforeach()
 
-####################
-##  Java package  ##
-####################
-file(GENERATE OUTPUT java/$<CONFIG>/replace.cmake
-  CONTENT
-  "FILE(READ ${PROJECT_SOURCE_DIR}/ortools/java/pom.xml.in input)
-STRING(REPLACE \"@PROJECT_VERSION@\" \"${PROJECT_VERSION}\" input \"\${input}\")
-STRING(REPLACE \"@ortools@\" \"$<TARGET_FILE:${PROJECT_NAME}>\" input \"\${input}\")
-STRING(REPLACE \"@native@\" \"$<TARGET_FILE:jniortools>\" input \"\${input}\")
-FILE(WRITE pom.xml \"\${input}\")"
-)
+#################################
+##  Java Native Maven Package  ##
+#################################
+configure_file(
+  ${PROJECT_SOURCE_DIR}/ortools/java/pom-native.xml.in
+  ${PROJECT_BINARY_DIR}/java/pom-native.xml.in
+  @ONLY)
 
 add_custom_command(
-  OUTPUT java/pom.xml
-  COMMAND ${CMAKE_COMMAND} -P ./$<CONFIG>/replace.cmake
-  WORKING_DIRECTORY java
-  )
+  OUTPUT java/${JAVA_NATIVE_PROJECT}/pom.xml
+  DEPENDS ${PROJECT_BINARY_DIR}/java/pom-native.xml.in
+  COMMAND ${CMAKE_COMMAND} -E make_directory ${JAVA_NATIVE_PROJECT}
+  COMMAND ${CMAKE_COMMAND} -E copy ./pom-native.xml.in ${JAVA_NATIVE_PROJECT}/pom.xml
+  BYPRODUCTS
+  java/${JAVA_NATIVE_PROJECT}
+  WORKING_DIRECTORY java)
 
-# Main Target
+add_custom_target(java_native_package
+  DEPENDS
+  java/${JAVA_NATIVE_PROJECT}/pom.xml
+  COMMAND ${CMAKE_COMMAND} -E remove_directory src
+  COMMAND ${CMAKE_COMMAND} -E make_directory ${JAVA_RESOURCES_PATH}/${NATIVE_IDENTIFIER}
+  COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:ortools> ${JAVA_RESOURCES_PATH}/${NATIVE_IDENTIFIER}/
+  COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:jniortools> ${JAVA_RESOURCES_PATH}/${NATIVE_IDENTIFIER}/
+  COMMAND ${MAVEN_EXECUTABLE} compile
+  COMMAND ${MAVEN_EXECUTABLE} package
+  COMMAND ${MAVEN_EXECUTABLE} install
+  WORKING_DIRECTORY java/${JAVA_NATIVE_PROJECT})
+
+##########################
+##  Java Maven Package  ##
+##########################
+configure_file(
+  ${PROJECT_SOURCE_DIR}/ortools/java/pom-local.xml.in
+  ${PROJECT_BINARY_DIR}/java/pom-local.xml.in
+  @ONLY)
+
+add_custom_command(
+  OUTPUT java/${JAVA_PROJECT}/pom.xml
+  DEPENDS ${PROJECT_BINARY_DIR}/java/pom-local.xml.in
+  COMMAND ${CMAKE_COMMAND} -E make_directory ${JAVA_PROJECT}
+  COMMAND ${CMAKE_COMMAND} -E copy ./pom-local.xml.in ${JAVA_PROJECT}/pom.xml
+  BYPRODUCTS
+  java/${JAVA_PROJECT}
+  WORKING_DIRECTORY java)
+
 add_custom_target(java_package ALL
   DEPENDS
-    ortools::ortools
-    Java${PROJECT_NAME}_proto
-    jniortools
-    java/pom.xml
+  java/${JAVA_PROJECT}/pom.xml
+  COMMAND ${CMAKE_COMMAND} -E copy_directory ${PROJECT_SOURCE_DIR}/ortools/java/com src/main/java/
+  COMMAND ${CMAKE_COMMAND} -E copy ${PROJECT_SOURCE_DIR}/ortools/java/Loader.java ${JAVA_PACKAGE_PATH}/
+  COMMAND ${MAVEN_EXECUTABLE} compile
   COMMAND ${MAVEN_EXECUTABLE} package
-  WORKING_DIRECTORY java
-  )
+  COMMAND ${MAVEN_EXECUTABLE} install
+  WORKING_DIRECTORY java/${JAVA_PROJECT})
+add_dependencies(java_package java_native_package)
 
-# Test
+#################
+##  Java Test  ##
+#################
+set(JAVA_TEST_PROJECT ortools-test)
 if(BUILD_TESTING)
+  configure_file(
+    ${PROJECT_SOURCE_DIR}/java/pom-test.xml.in
+    ${PROJECT_BINARY_DIR}/java/pom-test.xml.in
+    @ONLY)
+
+  add_custom_command(
+    OUTPUT java/${JAVA_TEST_PROJECT}/pom.xml
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${JAVA_TEST_PROJECT}
+    COMMAND ${CMAKE_COMMAND} -E copy ./pom-test.xml.in ${JAVA_TEST_PROJECT}/pom.xml
+    BYPRODUCTS
+    java/${JAVA_TEST_PROJECT}
+    WORKING_DIRECTORY java)
+
+  add_custom_target(java_test_package ALL
+    DEPENDS
+    java/${JAVA_TEST_PROJECT}/pom.xml
+    COMMAND ${CMAKE_COMMAND} -E remove_directory src
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${JAVA_PACKAGE_PATH}
+    COMMAND ${CMAKE_COMMAND} -E copy ${PROJECT_SOURCE_DIR}/java/Test.java ${JAVA_PACKAGE_PATH}/
+    COMMAND ${MAVEN_EXECUTABLE} compile
+    COMMAND ${MAVEN_EXECUTABLE} package
+    WORKING_DIRECTORY java/${JAVA_TEST_PROJECT})
+  add_dependencies(java_test_package java_package)
+
+  add_test(
+    NAME JavaTest
+    COMMAND ${MAVEN_EXECUTABLE} exec:java -Dexec.mainClass=com.google.ortools.Test
+    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/java/${JAVA_TEST_PROJECT})
   #add_subdirectory(examples/java)
 endif()
